9315dbd224b30c98ed7fd7df753560d91025b2a9
Davis <108017251+dwang618@users.noreply.github.com>
2025-01-01 16:35:34 -0500

    Add files via upload
    grpc client endpoint

---
+++ Grpc.ChatClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using Grpc.Chat;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7173");
+
+var client = new Chat.ChatClient(channel);
+
+Console.Write("Name: ");
+var name = Console.ReadLine();
+
+var reply = client.EnterChat(new EnterRequest { Name = name });
+
+var writeThread = new Thread(async () =>
+{
+    while (true)
+    {
+        await foreach (var message in reply.ResponseStream.ReadAllAsync())
+        {
+            Console.WriteLine(message.Message);
+        }
+    }
+});
+
+writeThread.Start();
+
+
+while (true)
+{
+    var message = Console.ReadLine();
+
+    Console.SetCursorPosition(0, Console.CursorTop - 1);
+    ClearLine();
+
+    client.SendMessage(new ChatMessage { Message = $"{DateTime.UtcNow}, {name}: {message}"});
+}
+
+void ClearLine()
+{
+    int currentLineCursor = Console.CursorTop;
+    Console.SetCursorPosition(0, Console.CursorTop);
+    Console.Write(new string(' ', Console.WindowWidth));
+    Console.SetCursorPosition(0, currentLineCursor);
+}
+
